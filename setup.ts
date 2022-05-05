96a5a3bd4aa147f55d68d902b0d1d893036c0e45
Aaron Pettengill <aaropett@amazon.com>
2022-05-05 15:38:56 -0400

    Add comment to setup file

--- setup.ts
+++ setup.ts
@@ -1,1 +1,7 @@
+/**
+ * This workaround was suggested here:
+ * https://github.com/testing-library/react-testing-library/issues/1061#issuecomment-1117450890
+ *
+ * This suppresses one error, but doesn't actually solve the issue with act.
+ */
 global.IS_REACT_ACT_ENVIRONMENT = true;
